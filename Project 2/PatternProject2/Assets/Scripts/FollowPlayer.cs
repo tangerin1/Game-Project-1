f12bd41ad61f61d102c24e680e926b5d93eb8eb0
madguy711 <44320778+madguy711@users.noreply.github.com>
2026-01-28 20:30:23 -0500

    Delay to chase to match timer

--- Project 2/PatternProject2/Assets/Scripts/FollowPlayer.cs
+++ Project 2/PatternProject2/Assets/Scripts/FollowPlayer.cs
@@ -1,21 +1,35 @@
 using UnityEngine;
+using System.Collections;
 
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
     public float speed = 2f;
+    public float delayTime = 10f;
+    private bool follow = false;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(StartFollowing());
+    }
+
+    IEnumerator StartFollowing()
+    {
+        yield return new WaitForSeconds(delayTime);
+        follow = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!follow)
+        {
+            return;
+        }
+
         float step = Time.deltaTime * speed;
-        // transform.position = Vector3.Lerp(transform.position, player.position, step);
         transform.position = Vector3.MoveTowards(transform.position, player.position, step);
     }
 }
