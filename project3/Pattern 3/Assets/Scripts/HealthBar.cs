6d7f9db803b1d4a4a549c2d3bf2ffe024a06a58a
Abdi <abdim4515@gmail.com>
2026-02-04 19:22:09 -0500

    Health bar added with script

---
+++ project3/Pattern 3/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+    [Header("Health Bar Settings")]
+    public float maxHealth = 100f;
+    public float currentHealth = 100f;
+
+    [Header("UI Elements")]
+
+    public UnityEngine.UI.Image healthBarFill;
+    public RectTransform notch;
+    // Start is called before the first frame 
+    // update
+    void Start()
+    {
+        UpdateHealthBar();
+        PositionNotch();
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - amount, 0, maxHealth);
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        float amount = currentHealth / maxHealth;
+        healthBarFill.fillAmount = amount;
+    }
+
+    void PositionNotch()
+    {
+        float half = 0.5f;
+
+        float width = healthBarFill.rectTransform.rect.width;
+
+        // move notch
+        notch.anchoredPosition = new UnityEngine.Vector2(width * half, 0f);
+    }
+}
