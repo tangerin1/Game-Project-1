6699f0de60e85100b7d904cefd1a68da561bd4a7
madguy711 <44320778+madguy711@users.noreply.github.com>
2026-02-05 16:41:55 -0500

    candle script

--- project3/Pattern 3/Assets/Scripts/CandleBehavior.cs
+++ project3/Pattern 3/Assets/Scripts/CandleBehavior.cs
@@ -4,31 +4,24 @@ public class CandleBehavior : MonoBehaviour
 {
     public ParticleSystem flame;
     public ParticleSystemRenderer flameRenderer;
+    public Light flameLight;
     [ColorUsage(true, true)]
     public Color flameColor = Color.lightGoldenRodYellow;
     public bool flameEnabled = true;
     private Material flameMaterial;
+    public Renderer candleRenderer;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         flameRenderer = flame.GetComponent<ParticleSystemRenderer>();
         flameMaterial = flameRenderer.material;
-        ChangeColor(flameColor);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ToggleFlame();
-        }
-
-        if (Input.GetKeyDown(KeyCode.K)) 
-        {
-            ChangeColor(Color.blue);
-        }
+        
     }
 
 
@@ -43,7 +36,11 @@ public class CandleBehavior : MonoBehaviour
         flameColor = color;
         var main = flame.main;
         main.startColor = color;
-        flameMaterial.SetColor("_Color", color);
+        flameMaterial.SetColor("_Color", color * 2f);
+        Color.RGBToHSV(color, out float h, out float s, out float v);
+        flameLight.color = Color.HSVToRGB(h, s, 1f);
+        Material candleMaterial = candleRenderer.material;
+        candleMaterial.SetColor("_EmissionColor", color * 1.5f);
         
     }
 }
